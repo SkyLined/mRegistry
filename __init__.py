11fbe926a9483db90c25fb05f98de4bc99e675cd
SkyLined <github@skylined.nl>
2021-02-11 13:26:30 +0100

    Move `__init__.py` code to `mRegistry.py`

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
-from cRegistryHive import cRegistryHive;
-from cRegistryHiveKey import cRegistryHiveKey;
-from cRegistryHiveKeyNamedValue import cRegistryHiveKeyNamedValue;
-from cRegistryValue import cRegistryValue;
+from .mRegistry import *;
 
 __all__ = [
   "cRegistryHive",
